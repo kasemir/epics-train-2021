38a96202b73552c8b802916c657be2fd59a11808
training <training@epics.org>
2021-09-14 07:58:07 -0400

    Hide long host name

--- etc/profile.d/00_bash.sh
+++ etc/profile.d/00_bash.sh
@@ -2,3 +2,10 @@ alias ll="ls -lh"
 alias la="ls -lah"
 alias ..="cd .."
 
+# Remove the long host name from prompt
+if [ `hostname` = "training-VirtualBox" ]
+then
+    export PS1="\[\e]0;\u@VB: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@VB\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+fi
+
+
