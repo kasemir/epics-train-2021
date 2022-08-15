d8c794834f14e813d627ad5295cb955d2d1b1972
training <training@epics.org>
2022-08-15 11:59:08 -0400

    Area Detector compression

---
+++ examples/AreaDetector/codec.sh
@@ -0,0 +1,8 @@
+caput 13SIM1:cam1:SizeX 100
+caput 13SIM1:cam1:SizeY 100
+caput 13SIM1:cam1:DataType UInt8
+caput 13SIM1:cam1:AcquirePeriod 0.500
+
+caput 13SIM1:cam1:Acquire 1
+caput 13SIM1:Pva1:EnableCallbacks 1
+caput 13SIM1:Pva1:NDArrayPort CODEC1
